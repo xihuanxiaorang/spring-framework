47da6e2f9674c79a354a32aac1571b5039ba93f6
liulei <15019474951@163.com>
2022-04-05 15:22:46 +0800

    debug ①演示基于多例setter注入的循环依赖，Spring无法解决，抛出BeanCurrentlyInCreationException异常

--- spring-source-debug/src/main/java/top/xiaorang/spring/MainTest.java
+++ spring-source-debug/src/main/java/top/xiaorang/spring/MainTest.java
@@ -1,10 +1,13 @@
 package top.xiaorang.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import top.xiaorang.spring.bean.A;
 
 public class MainTest {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		A a = ctx.getBean("a", A.class);
+		System.out.println(a);
 //		Person person = ctx.getBean(Person.class);
 //		System.out.println(person);
 //		Connection connection = ctx.getBean("connection", Connection.class);
